eebd3e0cefe6d23adf9eb00b9c96510b8d6f8b33
Mr.VenomHare <venomhare9@gmail.com>
2024-11-12 01:39:00 +0530

    trail 5

--- api/rss.js
+++ api/rss.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
         const xmlString = await response.text();
         const result = await parseStringPromise(xmlString);
         const items = result.feed.entry;
-        console.log(items[0]["media:group"][0]["media:thumbnail"]);
+        console.log(items[0]["media:group"][0]["media:thumbnail"][0]["$"].url);
         const formattedItems = items.map((item) => ({
             title: item.title[0],
             link: item.link[0].$.href,
