1c57b3681ece55398a691610ae597d4f50e975c0
Mr.VenomHare <venomhare9@gmail.com>
2024-11-12 02:18:24 +0530

    Final final fix

--- src/components/Pages/Showcase.tsx
+++ src/components/Pages/Showcase.tsx
@@ -24,7 +24,7 @@ const Showcase = () => {
             .then((fetchedVideos)=>{
                 console.log("Feteched",fetchedVideos);
                 setVideos(fetchedVideos);
-                setCurrentVideo(videos[0].link);
+                setCurrentVideo(fetchedVideos[0].link);
             })
             // .catch((err)=>{setError(err.message)});
 
