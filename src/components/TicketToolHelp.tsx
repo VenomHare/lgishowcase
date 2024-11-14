00847c89ad243041fef9b5e7c9898b98178bcf5e
Mr.VenomHare <venomhare9@gmail.com>
2024-11-14 17:51:05 +0530

    Edited Mobile UI

---
+++ src/components/TicketToolHelp.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TicketToolHelp = () => {
+    return (
+        <>
+            <div className='TicketParent'>
+                
+            </div>
+        </>
+    )
+}
+
+export default TicketToolHelp
